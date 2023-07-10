2a58440e28f8c145face468ecb7ff86a630718be
MakovWait <maxkv258@gmail.com>
2023-07-10 12:10:59 +0200

    add h_box_list_item

--- src/components/editors/editor_item.gd
+++ src/components/editors/editor_item.gd
@@ -1,6 +1,5 @@
-extends HBoxContainer
+extends HBoxListItem
 
-signal clicked
 #signal edited(data: A)
 signal removed
 
@@ -9,26 +8,16 @@ signal removed
 @onready var _title_label: Label = %TitleLabel
 @onready var _explore_button: Button = %ExploreButton
 
-var _is_hovering = false
-var _is_selected = false
 var _get_actions_callback: Callable
 
 
 func _ready():
+	super._ready()
 	$Favorite/FavoriteButton.texture_normal = get_theme_icon("Favorites", "EditorIcons")
 	
 	$Icon.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	$Icon.custom_minimum_size = Vector2(64, 64) * Config.EDSCALE
 	$Icon.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-	
-	mouse_entered.connect(func(): 
-		_is_hovering = true
-		queue_redraw()
-	)
-	mouse_exited.connect(func(): 
-		_is_hovering = false
-		queue_redraw()
-	)
 	
 	_title_label.add_theme_font_override(
 		"font", get_theme_font("title", "EditorFonts")
@@ -87,44 +76,6 @@ func get_actions():
 		return _get_actions_callback.call()
 	else:
 		return []
-
-
-func _input(event: InputEvent) -> void:
-	var mb = event as InputEventMouseButton
-	if mb and get_global_rect().has_point(event.position):
-		if mb.button_index == MOUSE_BUTTON_LEFT:
-			if mb.is_pressed(): 
-				clicked.emit()
-
-
-func _draw():
-	if _is_hovering:
-		draw_style_box(
-			get_theme_stylebox("hover", "Tree"),
-			Rect2(Vector2.ZERO, size)
-		)
-	
-	if _is_selected:
-		draw_style_box(
-			get_theme_stylebox("selected", "Tree"),
-			Rect2(Vector2.ZERO, size)
-		)
-	
-	draw_line(
-		Vector2(0, size.y + 1), 
-		Vector2(size.x, size.y + 1), 
-		get_theme_color("guide_color", "Tree")
-	)
-
-
-func select():
-	_is_selected = true
-	queue_redraw()
-
-
-func deselect():
-	_is_selected = false
-	queue_redraw()
 
 
 static func _make_button(text, icon, on_pressed):
---
+++ src/components/h_box_list_item/h_box_list_item.gd
@@ -0,0 +1,57 @@
+class_name HBoxListItem
+extends HBoxContainer
+
+signal clicked
+
+
+var _is_hovering = false
+var _is_selected = false
+
+
+func _ready() -> void:
+	mouse_entered.connect(func(): 
+		_is_hovering = true
+		queue_redraw()
+	)
+	mouse_exited.connect(func(): 
+		_is_hovering = false
+		queue_redraw()
+	)
+
+
+func _input(event: InputEvent) -> void:
+	var mb = event as InputEventMouseButton
+	if mb and get_global_rect().has_point(event.position):
+		if mb.button_index == MOUSE_BUTTON_LEFT:
+			if mb.is_pressed(): 
+				clicked.emit()
+
+
+func _draw():
+	if _is_hovering:
+		draw_style_box(
+			get_theme_stylebox("hover", "Tree"),
+			Rect2(Vector2.ZERO, size)
+		)
+	
+	if _is_selected:
+		draw_style_box(
+			get_theme_stylebox("selected", "Tree"),
+			Rect2(Vector2.ZERO, size)
+		)
+	
+	draw_line(
+		Vector2(0, size.y + 1), 
+		Vector2(size.x, size.y + 1), 
+		get_theme_color("guide_color", "Tree")
+	)
+
+
+func select():
+	_is_selected = true
+	queue_redraw()
+
+
+func deselect():
+	_is_selected = false
+	queue_redraw()
