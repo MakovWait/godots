9ef774e5af0a5cf617d3df4d7f313b85b6719834
MakovWait <maxkv258@gmail.com>
2023-07-12 21:07:37 +0200

    fix dir.path_is_valid

--- src/extensions/dir.gd
+++ src/extensions/dir.gd
@@ -22,7 +22,4 @@ static func remove_recursive(path):
 
 
 static func path_is_valid(abs_path):
-	if abs_path.ends_with("/"):
-		return DirAccess.dir_exists_absolute(abs_path)
-	else:
-		return FileAccess.file_exists(abs_path)
+	return DirAccess.dir_exists_absolute(abs_path) or FileAccess.file_exists(abs_path)
