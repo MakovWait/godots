cee432d6cd2e171419fc396db49c30ca73399ccb
MakovWait <maxkv258@gmail.com>
2023-07-19 16:03:40 +0200

    use colors to distinguish favorite.button_pressed

--- src/components/misc/favorite_button.gd
+++ src/components/misc/favorite_button.gd
@@ -2,6 +2,15 @@ extends TextureButton
 
 
 func _ready() -> void:
-	texture_normal = get_theme_icon("NonFavorite", "EditorIcons")
-	texture_pressed = get_theme_icon("Favorites", "EditorIcons")
+#	texture_normal = get_theme_icon("NonFavorite", "EditorIcons")
+#	texture_pressed = get_theme_icon("Favorites", "EditorIcons")
+	
+	texture_normal = get_theme_icon("Favorites", "EditorIcons")
 	toggle_mode = true
+
+	_update_modualate()
+	toggled.connect(func(_arg): _update_modualate())
+
+
+func _update_modualate():
+	modulate = Color(1, 1, 1, 1) if button_pressed else Color(1, 1, 1, 0.2)
