6859dcef4eee5598bb709506fc744f751cad507a
Festerdam <festerdam@posteo.net>
2023-08-22 20:03:19 +0100

    Implement checksum checking

--- src/components/editors/remote/remote_editor_download/remote_editor_download.gd
+++ src/components/editors/remote/remote_editor_download/remote_editor_download.gd
@@ -4,6 +4,8 @@ const uuid = preload("res://addons/uuid.gd")
 
 signal downloaded(abs_zip_path: String)
 
+const _CHECKSUM_FILENAME = "SHA512-SUMS.txt"
+
 var _retry_callback
 var _url: String
 var _fallback_url: String = "":
@@ -100,10 +102,21 @@ func start(url, target_abs_dir, file_name, fallback_url = ""):
 			if _fallback_url:
 				_mirror_switch_button.show()
 			_status.text = status
+			return
 		else:
-			_install_button.disabled = false
-			_status.text = "Ready to install"
-			downloaded.emit(_download.download_file)
+			_status.text = "Checking file integrity..."
+			if not await _check_file_integrity():
+				$AcceptErrorDialog.dialog_text = "Integrity check failed!\n" + \
+						"Retry or use another mirror."
+				$AcceptErrorDialog.popup_centered()
+				_retry_button.show()
+				if _fallback_url:
+					_mirror_switch_button.show()
+					return
+		
+		_install_button.disabled = false
+		_status.text = "Ready to install"
+		downloaded.emit(_download.download_file)
 	
 	assert(target_abs_dir.ends_with("/"))
 	print("Downloading " + url)
@@ -171,8 +184,55 @@ func _notification(what: int) -> void:
 		_remove_downloaded_file()
 
 
+## Checks integrity of the downloaded file by veryfing that the SHA512
+## checksum is correct.  Downloads SHA512-SUMS.txt to do so.[br]
+##
+## The authenticity of that checksum cannot be verified, however.[br]
+##
+## Failing to download SHA512-SUMS.txt is treated as success, because, again,
+## this method does not verify authenticity of the release.[br]
+##
+## Returns true on success and false on failure.
+func _check_file_integrity() -> bool:
+	var checksum_url: String = _url.get_base_dir().path_join(_CHECKSUM_FILENAME)
+	var checksum_downloader := HTTPRequest.new()
+	checksum_downloader.download_file = _target_abs_dir.get_base_dir() \
+			.path_join(_CHECKSUM_FILENAME)
+	checksum_downloader.timeout = 10
+	checksum_downloader.download_chunk_size = 2048
+	print("Downloading ", checksum_url)
+	add_child(checksum_downloader)
+	checksum_downloader.request(checksum_url)
+	var sig_result = await checksum_downloader.request_completed
+	var result = sig_result[0]
+	var response_code = sig_result[1]
+	if result != HTTPRequest.RESULT_SUCCESS or response_code != 200:
+		return true
+	
+	var globalized_directory_path = ProjectSettings.globalize_path(_target_abs_dir)
+	match OS.get_name():
+		"Linux", "OpenBSD", "FreeBSD", "NetBSD", "BSD":
+			var status = OS.execute("bash", ["-c", "cd " + globalized_directory_path
+					+ " && sha512sum -c --ignore-missing --status "
+					+ _CHECKSUM_FILENAME])
+			return status == 0 or status == 127
+		"Windows":
+			return true #TODO
+		"macOS":
+			return true #TODO
+		_:
+			return true
+	
+	checksum_downloader.queue_free()
+
+
 func _remove_downloaded_file():
 	if _download.download_file:
 		DirAccess.remove_absolute(
 			ProjectSettings.globalize_path(_download.download_file)
+		)
+	var sum_file_path = _target_abs_dir.path_join(_CHECKSUM_FILENAME)
+	if FileAccess.file_exists(sum_file_path):
+		DirAccess.remove_absolute(
+			ProjectSettings.globalize_path(sum_file_path)
 		)
