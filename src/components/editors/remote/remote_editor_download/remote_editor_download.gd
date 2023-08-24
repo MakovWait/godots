34b09c6c19e5898234a12649ed169d04620e51a3
Festerdam <festerdam@posteo.net>
2023-08-24 20:55:47 +0100

    Avoid hanging main thread

--- src/components/editors/remote/remote_editor_download/remote_editor_download.gd
+++ src/components/editors/remote/remote_editor_download/remote_editor_download.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 const uuid = preload("res://addons/uuid.gd")
 
 signal downloaded(abs_zip_path: String)
+signal integrity_check_completed(passed: bool)
 
 const _CHECKSUM_FILENAME = "SHA512-SUMS.txt"
 
@@ -21,6 +22,7 @@ var _fallback_url: String = "":
 		_fallback_url = new_fallback
 var _target_abs_dir: String
 var _file_name: String
+var _integrity_check_thread: Thread
 
 @onready var _progress_bar: ProgressBar = get_node("%ProgressBar")
 @onready var _status: Label = get_node("%Status")
@@ -41,6 +43,7 @@ func _ready() -> void:
 	
 	_retry_button.pressed.connect(func():
 		_remove_downloaded_file()
+		_integrity_check_thread = null
 		if _retry_callback:
 			_retry_callback.call()
 	)
@@ -53,6 +56,8 @@ func _ready() -> void:
 		assert(_fallback_url)
 		start(_fallback_url, _target_abs_dir, _file_name, _url)
 	)
+	
+	integrity_check_completed.connect(_on_integrity_check_completed)
 
 
 func start(url, target_abs_dir, file_name, fallback_url = ""):
@@ -102,21 +107,29 @@ func start(url, target_abs_dir, file_name, fallback_url = ""):
 			if _fallback_url:
 				_mirror_switch_button.show()
 			_status.text = status
-			return
 		else:
 			_status.text = "Checking file integrity..."
-			if not await _check_file_integrity():
-				$AcceptErrorDialog.dialog_text = "Integrity check failed!\n" + \
-						"Retry or use another mirror."
-				$AcceptErrorDialog.popup_centered()
-				_retry_button.show()
-				if _fallback_url:
-					_mirror_switch_button.show()
-					return
-		
-		_install_button.disabled = false
-		_status.text = "Ready to install"
-		downloaded.emit(_download.download_file)
+			var checksum_url: String = _url.get_base_dir().path_join(_CHECKSUM_FILENAME)
+			var checksum_downloader = HTTPRequest.new()
+			checksum_downloader.timeout = 10
+			checksum_downloader.download_chunk_size = 2048
+			checksum_downloader.download_file = _target_abs_dir.get_base_dir() \
+					.path_join(_CHECKSUM_FILENAME)
+			add_child(checksum_downloader)
+			print("Downloading ", checksum_url)
+			checksum_downloader.request(checksum_url)
+			var sig_result = await checksum_downloader.request_completed
+			var request_result = sig_result[0]
+			var request_response_code = sig_result[1]
+			checksum_downloader.queue_free()
+			if request_result != HTTPRequest.RESULT_SUCCESS \
+					or request_response_code != 200:
+				integrity_check_completed.emit(true, false)
+			else:
+				_dismiss_button.disabled = true
+				_dismiss_button.hide()
+				_integrity_check_thread = Thread.new()
+				_integrity_check_thread.start(_check_file_integrity)
 	
 	assert(target_abs_dir.ends_with("/"))
 	print("Downloading " + url)
@@ -192,30 +205,19 @@ func _notification(what: int) -> void:
 ## Failing to download SHA512-SUMS.txt is treated as success, because, again,
 ## this method does not verify authenticity of the release.[br]
 ##
-## Returns true on success and false on failure.
-func _check_file_integrity() -> bool:
-	var checksum_url: String = _url.get_base_dir().path_join(_CHECKSUM_FILENAME)
-	var checksum_downloader := HTTPRequest.new()
-	checksum_downloader.download_file = _target_abs_dir.get_base_dir() \
-			.path_join(_CHECKSUM_FILENAME)
-	checksum_downloader.timeout = 10
-	checksum_downloader.download_chunk_size = 2048
-	print("Downloading ", checksum_url)
-	add_child(checksum_downloader)
-	checksum_downloader.request(checksum_url)
-	var sig_result = await checksum_downloader.request_completed
-	var result = sig_result[0]
-	var response_code = sig_result[1]
-	if result != HTTPRequest.RESULT_SUCCESS or response_code != 200:
-		return true
-	
+## This is supposed to be run by a separate thread to avoid freezing the main
+## thread.  Emits [signal integrity_check_completed] when done, with
+## [code]passed[/code] set to [code]true[/code] if successful,
+## [code]false[/code] otherwise.
+func _check_file_integrity() -> void:
 	var globalized_directory_path = ProjectSettings.globalize_path(_target_abs_dir)
 	match OS.get_name():
 		"Linux", "OpenBSD", "FreeBSD", "NetBSD", "BSD":
 			var status = OS.execute("bash", ["-c", "cd " + globalized_directory_path
 					+ " && sha512sum -c --ignore-missing --status "
 					+ _CHECKSUM_FILENAME])
-			return status == 0 or status == 127
+			call_deferred("emit_signal", "integrity_check_completed",
+					status == 0 or status == 127)
 		"Windows":
 			var certutil_output = []
 			OS.execute("certutil", ["-hashfile",
@@ -223,25 +225,42 @@ func _check_file_integrity() -> bool:
 					"SHA512"], certutil_output)
 			var output_lines = certutil_output[0].split("\n")
 			if len(output_lines) <= 2:
-				return true
+				call_deferred("emit_signal", "integrity_check_completed", true)
 			
 			var obtained_sum = output_lines[1].strip_edges()
 			if not obtained_sum.is_valid_hex_number():
-				return true
+				call_deferred("emit_signal", "integrity_check_completed", true)
 			
 			var checksum_file_contents = FileAccess.open(_target_abs_dir.path_join(
 					_CHECKSUM_FILENAME), FileAccess.READ).get_as_text()
 			
-			if obtained_sum + "  " + _file_name in checksum_file_contents:
-				return true
-			else:
-				return false
+			call_deferred("emit_signal", "integrity_check_completed",
+					obtained_sum + "  " + _file_name in checksum_file_contents)
 		"macOS":
-			return true #TODO
+			call_deferred("emit_signal", "integrity_check_completed", true)
 		_:
-			return true
-	
-	checksum_downloader.queue_free()
+			call_deferred("emit_signal", "integrity_check_completed", true)
+
+
+func _on_integrity_check_completed(passed: bool, from_thread: bool = true) -> void:
+	assert(_integrity_check_thread == null or not _integrity_check_thread.is_alive())
+	_dismiss_button.show()
+	_dismiss_button.disabled = false
+	if _integrity_check_thread == null:
+		return
+	if passed:
+		_install_button.disabled = false
+		_status.text = "Ready to install"
+		downloaded.emit(_download.download_file)
+	else:
+		$AcceptErrorDialog.dialog_text = "Integrity check failed!\n" + \
+				"Retry or use another mirror."
+		$AcceptErrorDialog.popup_centered()
+		_retry_button.show()
+		if _fallback_url:
+			_mirror_switch_button.show()
+	_integrity_check_thread.wait_to_finish()
+	_integrity_check_thread = null
 
 
 func _remove_downloaded_file():
