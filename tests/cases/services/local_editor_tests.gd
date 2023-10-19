5a521f7e8581c944a6ddd31a1077667e8e07c92b
Artsiom Dubinevich <34747326+JillyMan@users.noreply.github.com>
2023-10-19 10:04:47 -0700

    Add unit tests for local editor filter_by_name_pattern. (#42)
    * Add unit tests for local editor filter_by_name_pattern.
    
    * Add more test cases.
    
    * fix name

---
+++ tests/cases/services/local_editor_tests.gd
@@ -0,0 +1,17 @@
+class_name LocalEditorTests
+extends GdUnitTestSuite
+
+const config_path = "res://tests/cases/services/assets/editors.cfg"
+
+func test_filter_by_name_pattern(name: String, expected: int, test_parameters:= [
+	["4.1", 3],
+	["4.1s", 1],
+	["4.1   s", 1],
+	["   4.1   s", 1],
+	["4.1 StAble", 1],
+	["invalid", 0],
+]):
+	var editors = LocalEditors.List.new(config_path)
+	editors.load()
+	var result = editors.filter_by_name_pattern(name)
+	assert(result.size() == expected)
