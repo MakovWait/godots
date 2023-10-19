48e3dce7ad8b3e4c5680840559f733ce8f842579
JillyMan <artyom543211@gmail.com>
2023-10-19 15:52:10 +0200

    Add unit tests for local editor filter_by_name_pattern.

---
+++ tests/cases/services/local_editor_tests.gd
@@ -0,0 +1,15 @@
+class_name LocalEditorTests
+extends GdUnitTestSuite
+
+const config_path = "res://tests/cases/services/assets/editors.cfg"
+
+func test_filter_by_name_return(name: String, expected: int, test_parameters:= [
+	["4.1", 3],
+	["4.1s", 1],
+	["4.1   s", 1],
+	["   4.1   s", 1],
+]):
+	var editors = LocalEditors.List.new(config_path)
+	editors.load()
+	var result = editors.filter_by_name_pattern(name)
+	assert(result.size() == expected)
