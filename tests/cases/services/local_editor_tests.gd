6a74e21892fe46f6b089021bd71b4d93ef12f81b
JillyMan <artyom543211@gmail.com>
2023-10-19 16:00:38 +0200

    fix name

--- tests/cases/services/local_editor_tests.gd
+++ tests/cases/services/local_editor_tests.gd
@@ -3,7 +3,7 @@ extends GdUnitTestSuite
 
 const config_path = "res://tests/cases/services/assets/editors.cfg"
 
-func test_filter_by_name_return(name: String, expected: int, test_parameters:= [
+func test_filter_by_name_pattern(name: String, expected: int, test_parameters:= [
 	["4.1", 3],
 	["4.1s", 1],
 	["4.1   s", 1],
