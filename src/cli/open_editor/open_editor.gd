d38b60a93ee77b64988e03a2d229500080479e5a
JillyMan <artyom543211@gmail.com>
2023-10-20 18:45:43 +0200

    Update error message.

--- src/cli/open_editor/open_editor.gd
+++ src/cli/open_editor/open_editor.gd
@@ -38,7 +38,7 @@ func _find_editor_by_proj_godot(project_path: String) -> LocalEditors.Item:
 		result = result if result else _find_editor_by_external_project(project_path)
 	
 		if not result:
-			Output.push("Editor not found because it was either not imported into godots or the `project.godot` file does not contain a `version_hint`.")
+			Output.push("Editor not found: either the project isn't bound to an editor or `project.godot` lacks a `version_hint`.")
 	else:
 		Output.push("Editor not found as path to `project.godot` does not exist.")
 
